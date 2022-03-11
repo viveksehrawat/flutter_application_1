22ac7158a93993bb31fcba5400daa1548b0c8295
Vivek Sehrawat <>
2022-03-11 15:35:41 +0530

    added cart icon

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/drawer.dart';
+
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
 
@@ -8,15 +10,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int counter = 5;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Sabka Bazar")),
+      appBar: AppBar(
+        title: Text("Sabka Bazar"),
+        actions: <Widget>[
+          // Using Stack to show Notification Badge
+          Stack(
+            children: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.shopping_basket),
+                  onPressed: () {
+                    setState(() {
+                      counter = 0;
+                    });
+                  }),
+              counter != 0
+                  ? new Positioned(
+                      right: 8,
+                      top: 8,
+                      child: new Container(
+                        padding: EdgeInsets.all(2),
+                        decoration: new BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        constraints: BoxConstraints(
+                          minWidth: 15,
+                          minHeight: 15,
+                        ),
+                        child: Text(
+                          '$counter',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    )
+                  : new Container()
+            ],
+          ),
+        ],
+      ),
       body: Center(
           child: Container(
         child: Text("Home Page"),
       )),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
